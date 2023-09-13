e068b75f566dce5364619abde17722a792a34665
CoolSnow <670696652@qq.com>
2023-09-13 10:38:00 +0800

    feat: update to flutter 3

--- lib/app/network/error_handlers.dart
+++ lib/app/network/error_handlers.dart
@@ -5,10 +5,10 @@ import 'package:dio/dio.dart';
 
 import '/app/network/exceptions/api_exception.dart';
 import '/app/network/exceptions/app_exception.dart';
-import '/app/network/exceptions/network_exception.dart';
 import '/app/network/exceptions/not_found_exception.dart';
 import '/app/network/exceptions/service_unavailable_exception.dart';
 import '/flavors/build_config.dart';
+import 'exceptions/network_exception.dart';
 
 Exception handleError(String error) {
   final logger = BuildConfig.instance.config.logger;
@@ -17,24 +17,28 @@ Exception handleError(String error) {
   return AppException(message: error);
 }
 
-Exception handleDioError(DioError dioError) {
+Exception handleDioError(DioException dioError) {
   switch (dioError.type) {
-    case DioErrorType.cancel:
+    case DioExceptionType.cancel:
       return AppException(message: "Request to API server was cancelled");
-    case DioErrorType.connectTimeout:
+    case DioExceptionType.connectionTimeout:
       return AppException(message: "Connection timeout with API server");
-    case DioErrorType.other:
+    case DioExceptionType.unknown:
       return NetworkException("There is no internet connection");
-    case DioErrorType.receiveTimeout:
+    case DioExceptionType.receiveTimeout:
       return TimeoutException("Receive timeout in connection with API server");
-    case DioErrorType.sendTimeout:
+    case DioExceptionType.sendTimeout:
       return TimeoutException("Send timeout in connection with API server");
-    case DioErrorType.response:
+    case DioExceptionType.badResponse:
       return _parseDioErrorResponse(dioError);
-  }
+    case DioExceptionType.badCertificate:
+      return AppException(message: "Bad certificate in connection with API server");
+    case DioExceptionType.connectionError:
+      return AppException(message: "Connection error with API server");
+  }  
 }
 
-Exception _parseDioErrorResponse(DioError dioError) {
+Exception _parseDioErrorResponse(DioException dioError) {
   final logger = BuildConfig.instance.config.logger;
 
   int statusCode = dioError.response?.statusCode ?? -1;
